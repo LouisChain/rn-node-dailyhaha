c2976dfbd5034b6f620f9ac5ae516e40f8fb9e9e
Louis Chain <trinhluu33@gmail.com>
2019-04-09 22:56:20 +0700

    add fb audiance network

--- dailyfunny/android/app/src/main/java/com/programfunnypics/MainApplication.java
+++ dailyfunny/android/app/src/main/java/com/programfunnypics/MainApplication.java
@@ -2,10 +2,14 @@ package com.programfunnypics;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -16,7 +20,15 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import suraj.tiwari.reactnativefbads.FBAdsPackage;
+
 public class MainApplication extends Application implements ReactApplication {
+
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
@@ -28,11 +40,13 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new FBAdsPackage(),
                     new RNDeviceInfo(),
                     new RNAdMobPackage(),
                     new RNCWebViewPackage(),
                     new VectorIconsPackage(),
-                    new RNGestureHandlerPackage()
+                    new RNGestureHandlerPackage(),
+                    new FBSDKPackage(mCallbackManager)
             );
         }
 
@@ -51,5 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+        AppEventsLogger.activateApp(this);
+        AudienceNetworkAds.initialize(this); // <-- add this
     }
 }
